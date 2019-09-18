e08ab79abf769c24f07edb61e4ed4559fbbf4854
Conor85750 <55500804+Conor85750@users.noreply.github.com>
2019-09-18 11:35:00 -0400

    spider now

--- 115conordycus.py
+++ 115conordycus.py
@@ -6,17 +6,36 @@ body.circle(20)
 
 numberlegs = 8
 leglong = 90
-distance = 315 / numberlegs
+distance = 369 / numberlegs
 body.pensize(5)
 
 loop = 0
 while (loop < numberlegs):
-  body.setheading(distance*numberlegs-45)
   body.goto(0,20)
   body.setheading(distance*loop)
   body.forward(leglong)
-  body.setheading(distance*numberlegs+45)
   loop = loop + 1
+
+body.penup()
+body.goto(-20,-20)
+body.pendown()
+body.pencolor("red")
+body.fillcolor("red")
+body.begin_fill()
+body.circle(10)
+body.end_fill()
+body.pencolor("black")
+body.circle(5)
+body.pencolor("red")
+body.fillcolor("red")
+body.begin_fill()
+body.penup()
+body.goto(10,-20)
+body.pendown()
+body.circle(10)
+body.pencolor("black")
+body.circle(5)
+body.end_fill()
 
 body.hideturtle()
 
